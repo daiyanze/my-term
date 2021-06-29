d105132804f83650b7b4b42bccef138e7e9bc695
daiyanze <ginoalex8964@yahoo.com>
2021-06-29 16:19:50 +0900

    fix: removed some unnecessary plugins

--- nvim/nvim.config.lua
+++ nvim/nvim.config.lua
@@ -2,19 +2,13 @@
 -- Customizations
 -- --------------
 
--- Custom Key mappings
-
 -- Tagbar
 vim.api.nvim_set_keymap("n", "<leader>t", ":TagbarToggle<CR>", {noremap = true, silent = true})
-
--- Minimap
-vim.api.nvim_set_keymap("n", "<leader>m", ":MinimapToggle<CR>", {noremap = true, silent = true})
 
 -- Which-key
 require("which-key").register(
   {
       ["t"] = "Tagbar",
-      ["m"] = "Minimap",
   },
   {
       mode = "n", -- NORMAL mode
@@ -27,37 +21,13 @@ require("which-key").register(
 )
 
 -- Custom Plugins
-
 require("packer").use('dstein64/nvim-scrollview')
 require("packer").use('preservim/tagbar')
 require("packer").use('karb94/neoscroll.nvim')
 require("neoscroll").setup()
 require("packer").use('martinda/Jenkinsfile-vim-syntax')
 require("packer").use('mg979/vim-visual-multi')
-require("packer").use('rcarriga/nvim-dap-ui')
--- require("packer").use {
---    'folke/todo-comments.nvim',
---    requires = 'nvim-lua/plenary.nvim',
--- }
--- require("todo-comments").setup()
 
 -- Theme Gruvbox
-require("packer").use {
-    'npxbr/gruvbox.nvim',
-    requires = 'rktjmp/lush.nvim',
-}
-
--- Theme Enable on Start
 vim.cmd([[colorscheme gruvbox]])
 
-require("packer").use('wfxr/minimap.vim')
-
-vim.g.minimap_highlight_range = 1
-vim.g.minimap_width = 10
-vim.g.minimap_auto_start = 0
-vim.g.minimap_auto_start_win_enter = 1
-vim.g.minimap_git_colors = 1
-vim.g.minimap_highlight = 'MinimapCurrentLine'
-
--- Minimap set highlight on current line
-vim.cmd([[highlight MinimapCurrentLine ctermfg=Green guifg=#50FA7B guibg=#32302f]])
