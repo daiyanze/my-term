4e265ebeabf9f6308ebc65f8beb4d82679127d50
daiyanze <ginoalex8964@yahoo.com>
2021-07-28 19:00:24 +0900

    [lvim] refactor: - change the global varible from O to lvim - add plugin nvim-spectre - optimize galaxyline colorscheme

--- nvim/lv-config.lua
+++ nvim/lv-config.lua
@@ -3,16 +3,27 @@
 --------------------------------------
 
 -- Theme Gruvbox
-O.colorscheme = 'gruvbox-material'
+lvim.colorscheme = 'gruvbox-material'
 
--- Javascript
-O.lang.tsserver.formatter = 'prettier'
-O.lang.tsserver.linter = 'eslint'
+-- Colorscheme for galaxyline
+-- Based on sainnhe/gruvbox-material
+-- https://github.com/sainnhe/gruvbox-material/blob/master/autoload/gruvbox_material.vim#L200
+local colors = lvim.builtin.galaxyline.colors
+colors.green = '#b0b846'
+colors.purple = '#d3869b'
+colors.magenta = '#db4740' -- bg_red
+colors.orange = '#f28534'
+colors.yellow = '#e9b143'
+colors.red = '#f2594b'
+colors.blue = '#80aa9e'
+colors.cyan = '#8bba7f' -- aqua
+colors.grey = '#928374' -- grey1
+colors.alt_bg = '#32302f' -- bg_statusline1
 
 -- Custom Plugins
-O.user_plugins = {
+lvim.plugins = {
   -- Theme
-  {'sainnhe/gruvbox-material'},
+  {'sainnhe/gruvbox-material', opt = true},
   -- Utils
   {'dstein64/nvim-scrollview'},
   {'martinda/Jenkinsfile-vim-syntax'},
@@ -22,6 +33,7 @@ O.user_plugins = {
   {'onsails/lspkind-nvim'},
   {'kdheepak/lazygit.nvim'},
   {'sindrets/diffview.nvim'},
+  {'windwp/nvim-spectre'},
   {'nacro90/numb.nvim',
     config = function()
       require('numb').setup()
@@ -42,7 +54,7 @@ O.user_plugins = {
 -- sainnhe/gruvbox-material configs
 vim.g.gruvbox_material_palette = 'mix'
 vim.g.gruvbox_material_enable_italic = 1
-vim.g.gruvbox_material_enable_bold = 1
+-- vim.g.gruvbox_material_enable_bold = 1
 
 -- lukas-reineke/indent-blankline.nvim configs
 vim.g.indent_blankline_char = '│'
